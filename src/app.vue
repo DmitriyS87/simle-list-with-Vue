6bd97468516f641a8d4a7505c2599ade2e5ce038
DmitriyS87 <schukindmitriy@gmail.com>
2019-06-19 20:45:45 +0800

    makes empty project with dependencies

---
+++ src/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>Table on Vue</h1>
+  </div>
+</template>
+<script>
+export default {
+  components: {}
+};
+</script>
+
+
+<style>
+#app {
+  margin: 0 auto;
+  line-height: 1.4;
+  font-family: "Roboto", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #191919;
+}
+</style>
